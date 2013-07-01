46f6581992a8044dedb2d186e28953cb127d2bca
MakerSquare Student <student@themakersquare.com>
2013-07-01 13:37:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KristaBlog::Application.config.secret_token = '922ec4d3f3b430b6c134967d10a6feffbe7cc0066c2f89635f94fc26ef6daee8d6d2059c40e64cb4794ef0ee34dda755f8ef9973701d72ad8c15a13f7b725d7c'
